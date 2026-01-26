4abfcc2fc1c6e5d38e9cf86995433c2301edd0a8
Kristina M <kristina0x7@gmail.com>
2026-01-26 22:40:08 +0100

    feat: paymentpersistenceexception

---
+++ src/main/java/com/example/payment/PaymentPersistenceException.java
@@ -0,0 +1,7 @@
+package com.example.payment;
+
+public class PaymentPersistenceException extends RuntimeException {
+    public PaymentPersistenceException(String message) {
+        super(message);
+    }
+}
