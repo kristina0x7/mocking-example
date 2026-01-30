4193d7012dd618638f35dd9814ad869f6ef5f764
Kristina M <kristina0x7@gmail.com>
2026-01-30 17:04:02 +0100

    add constructor

--- src/main/java/com/example/payment/PaymentGatewayClient.java
+++ src/main/java/com/example/payment/PaymentGatewayClient.java
@@ -1,4 +1,9 @@
 package com.example.payment;
 
 public class PaymentGatewayClient implements PaymentApiClient {
+    private final String apiKey;
+
+    public PaymentGatewayClient(String apiKey) {
+        this.apiKey = apiKey;
+    }
 }
