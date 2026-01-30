76a50efa422829b08ec437941a12b21efb92ebef
Kristina M <kristina0x7@gmail.com>
2026-01-30 18:55:58 +0100

    test: null paymentapiclient throws exc

--- src/test/java/com/example/payment/PaymentProcessorTest.java
+++ src/test/java/com/example/payment/PaymentProcessorTest.java
@@ -1,9 +1,14 @@
 package com.example.payment;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 class PaymentProcessorTest {
@@ -19,4 +24,20 @@ class PaymentProcessorTest {
 
     @InjectMocks
     private PaymentProcessor paymentProcessor;
+
+
+    @Nested
+    @DisplayName("Konstruktor-tester")
+    class ConstructorTests {
+
+        @Test
+        @DisplayName("Skapa PaymentProcessor med null PaymentApiClient - ska kasta NullPointerException")
+        void constructor_NullPaymentApiClient_ThrowsNullPointerException() {
+            assertThatThrownBy(() ->
+                    new PaymentProcessor(null, paymentRepository, emailSender))
+                    .isInstanceOf(NullPointerException.class)
+                    .hasMessageContaining("paymentApiClient cannot be null");
+        }
+    }
+
 }
