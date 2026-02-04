9bd4dd2f1712b5a352e1c61cbe0e495f31015f7a
Kristina M <kristina0x7@gmail.com>
2026-02-04 14:25:50 +0100

    red: test newCart_isEmpty

--- src/test/java/com/example/shop/ShoppingCartTest.java
+++ src/test/java/com/example/shop/ShoppingCartTest.java
@@ -1,4 +1,15 @@
 package com.example.shop;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 class ShoppingCartTest {
+
+    @Test
+    void newCart_isEmpty() {
+        ShoppingCart cart = new ShoppingCart();
+        assertTrue(cart.isEmpty());
     }
+}
